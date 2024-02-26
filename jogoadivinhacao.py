a6bfcb7ebc5722bb971ccb1ee2548dc63f4d52d6
Carla Dutra <100164378+carladutra@users.noreply.github.com>
2024-02-26 11:15:47 -0300

    Add files via upload

--- jogoadivinhacao.py
+++ jogoadivinhacao.py
@@ -2,14 +2,18 @@ print('*********************************')
 print('Bem vindo, ao JOGO DE ADIVINHAÇÃO')
 print('*********************************')
 
-numeroSecreto = 17
+numeroSecreto = 38
 
-chute = input('Digite um número')
+chuteString = input('Digite um número: ')
 
-print('Você digitou o número', chute)
+
+print('Você digitou o número', chuteString)
+
+chute = int(chuteString)
 
 if numeroSecreto == chute:
     print('Você acertou!')
 
 else:
     print('Você errou!')
+
