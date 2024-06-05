33172fac9a3b2268e9f518518ff45992e4265f9f
Carla Dutra <100164378+carladutra@users.noreply.github.com>
2024-06-05 09:05:37 -0300

    Add files via upload

--- forca.py
+++ forca.py
@@ -3,6 +3,7 @@ def jogar_forca():
     print('*********************************')
     print('Bem vindo, ao JOGO FORCA')
     print('*********************************')
+
     #Definir qual a palavra secreta
     palavraSecreta = "banana"
 
@@ -11,9 +12,15 @@ def jogar_forca():
 
     #Enquanto o jogador não se "enforcar" E não acertar a palavra, faça algo
     while(not enforcou and not acertou):
-        print("Jogando....")
+        chute = input("Escolha uma letra!")
+        for letra in palavraSecreta:
+            if(chute == letra):
+                print(letra)
+                
+        print("Escolha outra letra!")
 
 
     print("Fim de jogo!")
+
 if(__name__ == "__main__"):
     jogar_forca()
