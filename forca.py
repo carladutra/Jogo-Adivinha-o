50d50165c208fe6113d400b5cbe831801dc186f3
Carla Dutra <100164378+carladutra@users.noreply.github.com>
2024-07-24 09:06:14 -0300

    Add files via upload

--- forca.py
+++ forca.py
@@ -11,7 +11,7 @@ def jogar_forca():
 
     enforcou = False
     acertou = False
-    
+    erros = 0
 
     #Enquanto o jogador não se "enforcar" E não acertar a palavra, faça algo
     while(not enforcou and not acertou):
@@ -22,11 +22,21 @@ def jogar_forca():
         for letra in palavraSecreta:
             if(chute.upper() == letra.upper()):
                 letras_acertadas[index] = letra
-            index = index +1
+            index += 1 #index = index + 1
+
+        else:
+            erros += 1 #erros = erros + 1
+
+        enforcou = erros == 6
+        acertou = "_" not in letras_acertadas
         print(letras_acertadas)        
                 
         print("Escolha outra letra!")
 
+    if(acertou):
+        print("Você acertou!")
+    else:
+        print("Você perdeu o jogo!")
 
     print("Fim de jogo!")
 
